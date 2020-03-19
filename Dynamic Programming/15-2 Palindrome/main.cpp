517a74b10090b6cf8e656f196c36f97bf28c8857
fjxds <gongwz@sjtu.edu.cn>
2020-03-19 21:22:26 +0800

    15-2 Palindrome

--- Dynamic Programming/15-2 Palindrome/main.cpp
+++ Dynamic Programming/15-2 Palindrome/main.cpp
@@ -2,17 +2,17 @@
 #include <vector>
 #include <string>
 using namespace std;
-int *c;
+int **c;
 int maxx;
 int start;
 void preprocess(string s)
 {
 	for (int i=0;i<s.length();i++)
 	{
-		c[i]=1;
+		c[i][1]=1;
 		if (i<s.length()-1&&s[i]==s[i+1])
 		{
-			c[i]=2;
+			c[i][2]=1;
 			maxx=2;
 			start=i;
 		}
@@ -25,9 +25,9 @@ void longest(string s)
 		for (int i=0;i<=s.length()-l;i++)
 		{
 			int j=l+i-1;
-			if (s[i]==s[j]&&c[i+1]==l-2)
+			if (s[i]==s[j]&&c[i+1][l-2]==1)
 			{
-				c[i]=l;
+				c[i][l]=1;
 				maxx=l;
 				start=i;
 			}
@@ -40,7 +40,14 @@ int main(int argc, char** argv) {
 	string s;
 	cin>>s;
 	fclose(stdin);
-	c=new int[s.length()];
+	c=new int*[s.length()];
+	for (int i=0;i<s.length();i++)
+	{
+		c[i]=new int[s.length()+1];
+		for (int j=0;j<=s.length();j++)
+		c[i][j]=0;
+		c[i][1]=1;
+	}
 	preprocess(s);
 	longest(s);
 	cout<<s.substr(start,maxx)<<endl;
